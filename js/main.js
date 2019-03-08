341aa64d4d9fd8688e0c537228ffa84aa1d77deb
svennorge <svku70@gmx.net>
2019-03-08 17:12:41 +0100

    basic structure

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
+
+
 function startCharging() {
+    console.log('Start Charging')
     return true
 }
 
 function stopCharging() {
+    console.log('Stop Charging')
     return true
 }
 
 function loadConfig() {
+    console.log('Config Loaded')
     return true
 }
 
 function resetEVSE() {
+    console.log('Reset EVSE')
     return true
 }
+
